1db42ab5c760e2ca4b8cca7de32148d41d53c44e
Deluca00 <huynhkanh24@gmail.com>
2023-12-08 17:41:45 +0700

    ádfasd

--- DataAccess/AccountDAO.cs
+++ DataAccess/AccountDAO.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
-
 namespace DataAccess
 {
     public class Account
@@ -12,12 +11,30 @@ namespace DataAccess
         [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
 
+        // Change the property name from Password to HashedPassword
         [Required(ErrorMessage = "Password is required")]
-        public string Password{ get; set; }
+        public string HashedPassword { get; set; }
 
+        // Use this method to set the hashed password
+        public void SetPassword(string password)
+        {
+            // Hash the password using SHA256
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                HashedPassword = Convert.ToBase64String(hashedBytes);
+            }
+        }
+
+        // Use this method to verify the password
         public bool VerifyPassword(string password)
         {
-            return BCrypt.Net.BCrypt.Verify(password, Password);
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                string hashedInputPassword = Convert.ToBase64String(hashedBytes);
+                return string.Equals(hashedInputPassword, HashedPassword);
+            }
         }
     }
 }
--- ObjectBussiness/Account.cs
+++ ObjectBussiness/Account.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using BCrypt.Net;
 
 namespace ObjectBussiness
 {
@@ -15,24 +14,49 @@ namespace ObjectBussiness
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Display(Name = "Account ID")]
         public int AccountID { get; set; }
+
         [Display(Name = "Exam ID")]
         public int ExamID { get; set; }
+
         [ForeignKey("ExamRegister")]
         [Display(Name = "Exam Register ID")]
         public int ExamRegisterID { get; set; }
-        public string Password { get; set; }
+
+        // Change the property name from Password to HashedPassword
+        public string HashedPassword { get; set; }
+
         [JsonIgnore]
         public virtual Decentralization? Decentralization { get; set; }
+
         [JsonIgnore]
         public virtual ICollection<News>? News { get; set; }
+
         [JsonIgnore]
         public virtual Exam? Exam { get; set; }
+
         [JsonIgnore]
         public virtual ExamRegister? ExamRegister { get; set; }
 
+        // Use this method to set the hashed password
+        public void SetPassword(string password)
+        {
+            // Hash the password using SHA256
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                HashedPassword = Convert.ToBase64String(hashedBytes);
+            }
+        }
+
+        // Use this method to verify the password
         public bool VerifyPassword(string password)
         {
-            return BCrypt.Net.BCrypt.Verify(password, Password);
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                string hashedInputPassword = Convert.ToBase64String(hashedBytes);
+                return string.Equals(hashedInputPassword, HashedPassword);
+            }
         }
     }
 }
