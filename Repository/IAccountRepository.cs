4af5f21a1651abc23e43222f081d6d20aa9da553
Deluca00 <huynhkanh24@gmail.com>
2023-12-10 12:46:58 +0700

    update

---
+++ Repository/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    public interface IAccountRepository
+    {
+        void DeleteAccount(int id);
+        Account GetAccountById(int id);
+        IEnumerable<Account> GetAccounts();
+        void InsertAccount(Account account);
+        void UpdateAccount(Account account);
+    }
+}
