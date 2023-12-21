e61321486b7c6a12328fd8619e76b6a6fd7d4555
Deluca00 <huynhkanh24@gmail.com>
2023-12-21 11:43:36 +0700

    up

--- WebMVC/Areas/Admin/Controllers/logController.cs
+++ WebMVC/Areas/Admin/Controllers/logController.cs
@@ -1,27 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using System.Buffers.Text;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data.Entity.Infrastructure;
 
 namespace WebMVC.Areas.Admin.Controllers
 {
-    public class logController : Controller
+    public class LogController : Controller
     {
-		[HttpGet]
-		public IActionResult logup()
+        private DbModel db = new DbModel();
+        [HttpGet]
+        public IActionResult Logup()
         {
             return View();
         }
-		
-		[HttpGet]
-        public IActionResult login()
-		{
-			return View();
-		}
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
         [HttpPost]
-		public IActionResult login(string username ,string password)
-		{
-			var use = username;
-			var pas = password;
-			return View();
-		}
-     }   
+        public IActionResult Login(string username, string password)
+        {
+           
+            var use = username;
+            var pas = password;
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Logup(string candidatename, string age, string gender, DateTime dateofbirth, string phonenumber, string email, string placeofbirth, string residentialaddress, string city, string country)
+        {
+
+            var candidateNameVar = candidatename;
+            var ageVar = age;
+            var genderVar = gender;
+            var dateOfBirthVar = dateofbirth;
+            var phoneNumberVar = phonenumber;
+            var emailVar = email;
+            var placeOfBirthVar = placeofbirth;
+            var residentialAddressVar = residentialaddress;
+            var cityVar = city;
+            var countryVar = country;
+
+         
+            return RedirectToAction("Index", "Home");
+        }
+    }
 }
+
